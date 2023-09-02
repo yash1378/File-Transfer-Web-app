5aad87cbd22a0048747dd127338449219d1d6c3c
yash <slayycasm997@gmail.com>
2023-09-02 07:52:53 +0530

    Code updated with new features and issues resolved

--- index.js
+++ index.js
@@ -1,71 +1,15 @@
-// const express = require('express');
-// const os = require('os');
-// const fileUpload = require('express-fileupload');
-// const cors = require('cors'); // Import the cors package
-// const app = express();
-// const port = 3300;
-
-// // Use the cors middleware to allow all origins (for local development)
-// app.use(cors());
-// // Get the local IP address of the server
-// const localIpAddress = Object.values(os.networkInterfaces())
-//   .flat()
-//   .find((iface) => iface.family === 'IPv4' && !iface.internal)
-//   .address;
-
-
-// app.use(fileUpload());
-  
-// app.use(express.static('./public'));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/public/index.html');
-// });
-
-// // Serve files from the specified directory
-// app.use('/uploads', express.static('C:/Users/HP/Desktop/New folder/Referrrrer/app'));
-
-// app.post('/upload', (req, res) => {
-//   if (!req.files || Object.keys(req.files).length === 0) {
-//     return res.status(400).send('No files were uploaded.');
-//   }
-
-//   const uploadedFile = req.files.file;
-
-//   // Use forward slashes in the path
-//   uploadedFile.mv('C:/Users/HP/Desktop/New folder/Referrrrer/app/' + uploadedFile.name, (err) => {
-//     if (err) {
-//       return res.status(500).send(err);
-//     }
-
-//     const uploadedFilePath = `/uploads/${uploadedFile.name}`;
-//     // res.json({ fileUrl: uploadedFilePath });
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.end(`<html><body><a href="http://${localIpAddress}:${port}${uploadedFilePath}">Download File</a>
-//     </body></html>`);
-//   });
-// });
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running at http://${localIpAddress}:${port}`);
-// });
-
-
-
-
-
-const express = require('express');
-const os = require('os');
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
-const http = require('http');
-const socketIo = require('socket.io');
-
+const express = require("express");
+const os = require("os");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-
+const membersList = [];
+const userList = [];
+const userSocketMap = {};
 const port = 3300;
 
 // Use the cors middleware to allow all origins (for local development)
@@ -74,56 +18,121 @@ app.use(cors());
 // Get the local IP address of the server
 const localIpAddress = Object.values(os.networkInterfaces())
   .flat()
-  .find((iface) => iface.family === 'IPv4' && !iface.internal)
-  .address;
+  .find((iface) => iface.family === "IPv4" && !iface.internal).address;
 
 app.use(fileUpload());
+app.use(express.static("./public"));
 
-app.use(express.static('./public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 // Serve files from the specified directory
-app.use('/uploads', express.static('C:/Users/HP/Desktop/New folder/Referrrrer/app'));
+app.use(
+  "/uploads",
+  express.static("C:/Users/HP/Desktop/New folder/Referrrrer/app")
+);
 
-app.post('/upload/:room', (req, res) => {
+app.post("/upload/:room", (req, res) => {
   const room = req.params.room;
+  console.log(req.body);
+  console.log(req.body.sendlist);
+  const sendList = req.body.sendlist.split(",");
+  console.log(sendList[0]);
+  console.log(sendList[1]);
+
+  // Parse the list of names from the request body
 
   if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
+    return res.status(400).send("No files were uploaded.");
   }
 
   const uploadedFile = req.files.file;
 
-  uploadedFile.mv('C:/Users/HP/Desktop/New folder/Referrrrer/app/' + uploadedFile.name, (err) => {
-    if (err) {
-      return res.status(500).send(err);
+  uploadedFile.mv(
+    "C:/Users/HP/Desktop/New folder/Referrrrer/app/" + uploadedFile.name,
+    (err) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      const uploadedFilePath = `/uploads/${uploadedFile.name}`;
+
+
+
+      // Assuming you send it as an array of names
+      // Emit the fileUploaded event to specific users
+      sendList.forEach((userName) => {
+        const socketId = userSocketMap[userName];
+        if (socketId) {
+          io.to(socketId).emit("fileUploaded", {
+            filePath: uploadedFilePath,
+            room,
+          });
+          io.to(socketId).emit("link", { filePath: uploadedFilePath, room });
+        }
+      });
+
+      // io.to(room).emit("fileUploaded", { filePath: uploadedFilePath, room });
+      // io.to(room).emit("link", { filePath: uploadedFilePath, room });
+
+      return res.status(200).send("File uploaded to room: " + room);
     }
-
-    const uploadedFilePath = `/uploads/${uploadedFile.name}`;
-    io.to(room).emit('fileUploaded', { filePath: uploadedFilePath, room });
-
-    return res.status(200).send('File uploaded to room: ' + room);
-  });
+  );
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-  socket.on('joinRoom', (room) => {
-    socket.join(room);
-    console.log(`User joined room: ${room}`);
+  socket.on("joinRoom", (data) => {
+    // Add the new user to the list of members
+    socket.userName = data.n;
+    socket.roomNo = data.r;
+    membersList.push(data.n);
+    // Add the user to the userList
+    userList.push({ name: data.n, room: data.r });
+    socket.join(data.r);
+    io.to(data.r).emit("jointoast", data.n);
+    io.to(data.r).emit("userJoined", data.n);
+    userSocketMap[data.n] = socket.id;
+    // Send the updated list of members to all clients in the room
+    io.to(data.r).emit(
+      "updateMembers",
+      membersList.filter((member) =>
+        userList.some((user) => user.room === data.r && user.name === member)
+      )
+    );
+    console.log(`User joined room: ${data.r}`);
   });
 
-  socket.on('sendMessage', (data) => {
-    const { room, message } = data;
-    io.to(room).emit('messageReceived', message);
+  // When a user disconnects
+  socket.on("disconnect", () => {
+    // Identify the userName and roomNo of the disconnected user
+    const userName = socket.userName; // Assuming you have set this during connection
+    const roomNo = socket.roomNo; // Assuming you have set this during connection
+    console.log(roomNo);
+
+    // Find and remove the disconnected user from the userList
+    const userIndex = userList.findIndex(
+      (user) => user.name === userName && user.room === roomNo
+    );
+    if (userIndex !== -1) {
+      userList.splice(userIndex, 1);
+    }
+
+    // Create a new list of members for the specific room
+    const roomMembers = userList.filter((user) => user.room === roomNo);
+    console.log(roomMembers);
+
+    // Extract only the 'name' attribute from the roomMembers list
+    const memberNames = roomMembers.map((user) => user.name);
+    delete userSocketMap[userName];
+    // Emit the updated member list (names only) to all users in the room
+    io.to(roomNo).emit("updateMembers", memberNames);
+    io.to(roomNo).emit("left", userName);
   });
 });
 
 server.listen(port, () => {
   console.log(`Server is running at http://${localIpAddress}:${port}`);
 });
-
--- public/script.js
+++ public/script.js
@@ -1,52 +1,213 @@
-
 const socket = io();
+// Initialize an array to store selected member names
+const selectedMembers = [];
+
 
 // Join a room when the button is clicked
-document.getElementById('joinRoomButton').addEventListener('click', () => {
-  const room = document.getElementById('roomInput').value;
-  socket.emit('joinRoom', room);
-});
+document.getElementById("joinRoomButton").addEventListener("click", () => {
+  const room = document.getElementById("roomInput").value;
+  const Name = document.getElementById("nameInput").value;
 
-// Upload a file
-document.getElementById('uploadForm').addEventListener('submit', (event) => {
-  event.preventDefault();
-  const room = document.getElementById('roomInput').value;
-  const formData = new FormData(event.target);
-  formData.append('room', room);
+  socket.emit("joinRoom", { r: room, n: Name });
 
-  fetch('/upload/' + room, {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => response.text())
-  .then(data => {
-    console.log(data);
+  const joinbtn = document.getElementById("joinRoomButton");
+  const roominput = document.getElementById("roomInput");
+  const nameinput = document.getElementById("nameInput");
+  const list = document.getElementById("list");
+
+  // Toggle the visibility of the form
+  if (joinbtn.style.display === "none") {
+    joinbtn.style.display = "block"; // Show the form
+    roominput.style.display = "block";
+    nameinput.style.display = "block";
+    list.style.display = "none";
+  } else {
+    joinbtn.style.display = "none"; // Show the form
+    roominput.style.display = "none";
+    nameinput.style.display = "none";
+    list.style.display = "block";
+  }
+
+
+
+  socket.on("updateMembers", (membersList) => {
+    // Update the HTML to display the list of members
+    const membersListContainer = document.getElementById("membersListContainer");
+    membersListContainer.innerHTML = "";
+  
+    membersList.forEach((member) => {
+      // Create a list item element
+      const memberItem = document.createElement("li");
+  
+      // Create a checkbox element
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.value = member;
+      checkbox.name = "selectedMembers"; // You can give it a unique name
+      checkbox.className = "member-checkbox"; // Add the class "member-checkbox"
+  
+      // Create a label for the checkbox with the member's name
+      const label = document.createElement("label");
+      label.textContent = member;
+  
+      // Append the checkbox and label to the list item
+      memberItem.appendChild(checkbox);
+      memberItem.appendChild(label);
+  
+      // Append the list item to the membersListContainer
+      membersListContainer.appendChild(memberItem);
+  
+      // Add an event listener for the checkbox
+      checkbox.addEventListener("change", () => {
+        handleCheckboxChange(checkbox);
+      });
+    });
+  });
+  
+  
+
+
+  socket.on("jointoast", (msg) => {
+    const toastContainer = document.createElement("div");
+    toastContainer.id = "toast-bottom-right";
+    toastContainer.className =
+      "fixed flex items-center w-full max-w-xs p-4 space-x-4 text-white bg-green-500 divide-x divide-gray-200 rounded-lg shadow right-5 bottom-5 ";
+    toastContainer.setAttribute("role", "alert");
+
+    const toastContent = document.createElement("div");
+    toastContent.className = "text-sm font-normal";
+    toastContent.textContent = `${msg} has Joined the Room`;
+
+    toastContainer.appendChild(toastContent);
+
+    document.body.appendChild(toastContainer);
+
+    // Remove the toast after 3 seconds
+    setTimeout(function () {
+      document.body.removeChild(toastContainer);
+    }, 3000);
   });
 });
 
+// Upload a file
+document.getElementById("uploadForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const room = document.getElementById("roomInput").value;
+  const formData = new FormData(event.target);
+  formData.append("room", room);
+  // socket.emit('sendlist',selectedMembers);
+  formData.append("sendlist",selectedMembers);
+
+  fetch("/upload/" + room, {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data);
+    });
+});
+
 // Receive file upload notifications
-socket.on('fileUploaded', (data) => {
-  const message = `File uploaded to room: ${data.room}\nDownload: ${data.filePath}`;
-  document.getElementById('messages').innerText += message + '\n';
+socket.on("fileUploaded", (data) => {
+  const toastContainer = document.createElement("div");
+  toastContainer.id = "toast-bottom-right";
+  toastContainer.className =
+    "fixed flex items-center w-full max-w-xs p-4 space-x-4 text-white bg-blue-500 divide-x divide-gray-200 rounded-lg shadow right-5 bottom-5 ";
+  toastContainer.setAttribute("role", "alert");
+
+  const toastContent = document.createElement("div");
+  toastContent.className = "text-sm font-normal";
+  toastContent.textContent = "A File has been Uploaded";
+
+  toastContainer.appendChild(toastContent);
+
+  document.body.appendChild(toastContainer);
+
+  // Remove the toast after 3 seconds
+  setTimeout(function () {
+    document.body.removeChild(toastContainer);
+  }, 3000);
 });
 
 // Receive messages
-socket.on('messageReceived', (message) => {
-  document.getElementById('messages').innerText += message + '\n';
+socket.on("messageReceived", (message) => {
+  document.getElementById("messages").innerText += message + "\n";
 });
 
-// Receive file upload notifications
-socket.on('fileUploaded', (data) => {
-const message = `File uploaded to room: ${data.room}\n`;
-const downloadLink = document.createElement('a');
-downloadLink.href = data.filePath; // This should be the path to the uploaded file on the server
-downloadLink.download = data.filePath.split('/').pop(); // Extract the filename
-downloadLink.textContent = 'Download File';
+socket.on("link", (data) => {
+  const message = `File uploaded to room: ${data.room} `;
 
-// Append the message and download link to the messages div
-const messagesDiv = document.getElementById('messages');
-messagesDiv.appendChild(document.createTextNode(message));
-messagesDiv.appendChild(downloadLink);
-messagesDiv.appendChild(document.createElement('br')); // Add line break
+  // Create a new list item for the message
+  const listItem = document.createElement("li");
+  listItem.textContent = message;
+
+  // Create a download link for the file
+  const downloadLink = document.createElement("a");
+  downloadLink.href = data.filePath; // This should be the path to the uploaded file on the server
+  downloadLink.download = data.filePath.split("/").pop(); // Extract the filename
+  downloadLink.textContent = "Download File";
+
+  // Append the download link to the list item
+  listItem.appendChild(downloadLink);
+
+  // Append a line break after the list item
+  listItem.appendChild(document.createElement("br"));
+
+  // Append the list item to the messages list
+  const messagesList = document.getElementById("messages");
+  messagesList.appendChild(listItem);
+
+  // Make sure the box is visible when a file is uploaded
+  const box = document.getElementById("box");
+  box.style.display = "block";
 });
+
+socket.on("left", (NAAM) => {
+  const toastContainer = document.createElement("div");
+  toastContainer.id = "toast-bottom-right";
+  toastContainer.className =
+    "fixed flex items-center w-full max-w-xs p-4 space-x-4 text-white bg-red-500 divide-x divide-gray-200 rounded-lg shadow right-5 bottom-5 ";
+  toastContainer.setAttribute("role", "alert");
+
+  const toastContent = document.createElement("div");
+  toastContent.className = "text-sm font-normal";
+  toastContent.textContent = `${NAAM} has left your room`;
+
+  toastContainer.appendChild(toastContent);
+
+  document.body.appendChild(toastContainer);
+
+  // Remove the toast after 3 seconds
+  setTimeout(function () {
+    document.body.removeChild(toastContainer);
+  }, 3000);
+});
+
+function handleCheckboxChange(checkbox) {
+  if (checkbox.checked) {
+    // If checkbox is checked, add the name to the selectedMembers array
+    selectedMembers.push(checkbox.value);
+  } else {
+    // If checkbox is unchecked, remove the name from the selectedMembers array
+    const index = selectedMembers.indexOf(checkbox.value);
+    if (index !== -1) {
+      selectedMembers.splice(index, 1);
+    }
+  }
+}
+
+// Add an event listener to the "Select All" checkbox
+const selectAllCheckbox = document.getElementById("selectAllMembers");
+selectAllCheckbox.addEventListener("change", function () {
+  const checkboxes = document.querySelectorAll(".member-checkbox");
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = selectAllCheckbox.checked;
+    handleCheckboxChange(checkbox);
+  });
+});
+
+
+
+
 
